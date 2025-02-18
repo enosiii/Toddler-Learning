6f74f453a8ed95fc8dd1e871f64d90eb587610e3
enosiii <121915517+enosiii@users.noreply.github.com>
2025-02-18 12:27:08 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,34 +3,34 @@
 // Data for each activity
 const activities = {
     colors: [
-        { name: 'Red', image: 'assets/images/red.png', sound: 'assets/sounds/Red.mp3' },
-        { name: 'Blue', image: 'assets/images/blue.png', sound: 'assets/sounds/Blue.mp3' },
-        { name: 'Green', image: 'assets/images/green.png', sound: 'assets/sounds/Green.mp3' },
-        { name: 'Yellow', image: 'assets/images/yellow.png', sound: 'assets/sounds/Yellow.mp3' }
+        { name: 'Red', image: '/Toddler-Learning/assets/images/red.png', sound: '/Toddler-Learning/assets/sounds/Red.mp3' },
+        { name: 'Blue', image: '/Toddler-Learning/assets/images/blue.png', sound: '/Toddler-Learning/assets/sounds/Blue.mp3' },
+        { name: 'Green', image: '/Toddler-Learning/assets/images/green.png', sound: '/Toddler-Learning/assets/sounds/Green.mp3' },
+        { name: 'Yellow', image: '/Toddler-Learning/assets/images/yellow.png', sound: '/Toddler-Learning/assets/sounds/Yellow.mp3' }
     ],
     shapes: [
-        { name: 'Circle', image: 'assets/images/circle.png', sound: 'assets/sounds/Circle.mp3' },
-        { name: 'Square', image: 'assets/images/square.png', sound: 'assets/sounds/Square.mp3' },
-        { name: 'Triangle', image: 'assets/images/triangle.png', sound: 'assets/sounds/Triangle.mp3' },
-        { name: 'Star', image: 'assets/images/star.png', sound: 'assets/sounds/Star.mp3' }
+        { name: 'Circle', image: '/Toddler-Learning/assets/images/circle.png', sound: '/Toddler-Learning/assets/sounds/Circle.mp3' },
+        { name: 'Square', image: '/Toddler-Learning/assets/images/square.png', sound: '/Toddler-Learning/assets/sounds/Square.mp3' },
+        { name: 'Triangle', image: '/Toddler-Learning/assets/images/triangle.png', sound: '/Toddler-Learning/assets/sounds/Triangle.mp3' },
+        { name: 'Star', image: '/Toddler-Learning/assets/images/star.png', sound: '/Toddler-Learning/assets/sounds/Star.mp3' }
     ],
     animals: [
-        { name: 'Cat', image: 'assets/images/cat.png', sound: 'assets/sounds/CatNS.mp3' },
-        { name: 'Dog', image: 'assets/images/dog.png', sound: 'assets/sounds/DogNS.mp3' },
-        { name: 'Chicken', image: 'assets/images/rooster.png', sound: 'assets/sounds/RoosterNS.mp3' },
-        { name: 'Cow', image: 'assets/images/cow.png', sound: 'assets/sounds/CowNS.mp3' }
+        { name: 'Cat', image: '/Toddler-Learning/assets/images/cat.png', sound: '/Toddler-Learning/assets/sounds/CatNS.mp3' },
+        { name: 'Dog', image: '/Toddler-Learning/assets/images/dog.png', sound: '/Toddler-Learning/assets/sounds/DogNS.mp3' },
+        { name: 'Chicken', image: '/Toddler-Learning/assets/images/rooster.png', sound: '/Toddler-Learning/assets/sounds/RoosterNS.mp3' },
+        { name: 'Cow', image: '/Toddler-Learning/assets/images/cow.png', sound: '/Toddler-Learning/assets/sounds/CowNS.mp3' }
     ],
     numbers: [
-        { name: 'One', image: 'assets/images/1.png', sound: 'assets/sounds/One.mp3' },
-        { name: 'Two', image: 'assets/images/2.png', sound: 'assets/sounds/Two.mp3' },
-        { name: 'Three', image: 'assets/images/3.png', sound: 'assets/sounds/Three.mp3' },
-        { name: 'Four', image: 'assets/images/4.png', sound: 'assets/sounds/Four.mp3' },
-        { name: 'Five', image: 'assets/images/5.png', sound: 'assets/sounds/Five.mp3' },
-        { name: 'Six', image: 'assets/images/6.png', sound: 'assets/sounds/Six.mp3' },
-        { name: 'Seven', image: 'assets/images/7.png', sound: 'assets/sounds/Seven.mp3' },
-        { name: 'Eight', image: 'assets/images/8.png', sound: 'assets/sounds/Eight.mp3' },
-        { name: 'Nine', image: 'assets/images/9.png', sound: 'assets/sounds/Nine.mp3' },
-        { name: 'Ten', image: 'assets/images/10.png', sound: 'assets/sounds/Ten.mp3' }
+        { name: 'One', image: '/Toddler-Learning/assets/images/1.png', sound: '/Toddler-Learning/assets/sounds/One.mp3' },
+        { name: 'Two', image: '/Toddler-Learning/assets/images/2.png', sound: '/Toddler-Learning/assets/sounds/Two.mp3' },
+        { name: 'Three', image: '/Toddler-Learning/assets/images/3.png', sound: '/Toddler-Learning/assets/sounds/Three.mp3' },
+        { name: 'Four', image: '/Toddler-Learning/assets/images/4.png', sound: '/Toddler-Learning/assets/sounds/Four.mp3' },
+        { name: 'Five', image: '/Toddler-Learning/assets/images/5.png', sound: '/Toddler-Learning/assets/sounds/Five.mp3' },
+        { name: 'Six', image: '/Toddler-Learning/assets/images/6.png', sound: '/Toddler-Learning/assets/sounds/Six.mp3' },
+        { name: 'Seven', image: '/Toddler-Learning/assets/images/7.png', sound: '/Toddler-Learning/assets/sounds/Seven.mp3' },
+        { name: 'Eight', image: '/Toddler-Learning/assets/images/8.png', sound: '/Toddler-Learning/assets/sounds/Eight.mp3' },
+        { name: 'Nine', image: '/Toddler-Learning/assets/images/9.png', sound: '/Toddler-Learning/assets/sounds/Nine.mp3' },
+        { name: 'Ten', image: '/Toddler-Learning/assets/images/10.png', sound: '/Toddler-Learning/assets/sounds/Ten.mp3' }
     ]
 };
 
